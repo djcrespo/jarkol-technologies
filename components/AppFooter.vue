6cab262f89e9b181e46da75d5f3fdce31f921367
DJCrespo <dj.crespo.castilla@gmail.com>
2024-04-23 12:29:14 -0600

    update footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,63 +1,34 @@
 <template>
-    <footer class="pt-20 bg-gray-100 dark:bg-body border-t border-gray-200 dark:border-gray-100/20">
-        <AtomsContainer class-name="pb-10">
-            <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-8 gap-y-10">
-                <div class="col-span-2 md:col-span-4 lg:col-span-2">
-                    <NuxtLink to="/" class=" flex items-center gap-2">
-                      <!--
-                        <span class="flex">
-                            <span class="w-3 h-6 rounded-l-full flex bg-primary"></span>
-                            <span class="w-3 h-6 rounded-r-full flex bg-[#f88fc2] mt-2"></span>
-                        </span>
-                        <span class="text-xl font-bold text-gray-700 dark:text-white">Podux</span>
-                      -->
-                      <img class="h-8 w-auto" src="/jarkol/logo/jarkol.png" alt="Jarkol Technologies">
-                    </NuxtLink>
-
-                    <!--
-                    <p class="text-gray-600 dark:text-gray-300 pt-6">
-                        Jarkol Technologies.
-                    </p>
-                    -->
-                </div>
-
-                <BlocksFooterNavBlock title="Contacto" :links="quickLinks" />
-                <BlocksFooterNavBlock title="Servicios" :links="ressourcesLinks" />
-                <BlocksFooterNavBlock title="Productos" :links="supportLinks" />
-            </div>
-        </AtomsContainer>
-      <!--
-        <div class="py-2 bg-gray-200 dark:bg-[rgb(10,16,29)]">
+    <footer class="bg-gray-100 dark:bg-body border-t border-gray-200 dark:border-gray-100/20">
+      <div class="py-10">
             <AtomsContainer>
                 <div class="flex justify-between items-center gap-5 text-gray-600 dark:text-gray-300">
-                    <div class="">
-                        &copy; {{ new Date().getFullYear() }} Allright Reserved. Designed by <NuxtLink target="_blank"
+                    <div>
+                      <NuxtLink to="/">
+                        <img class="h-8 w-auto" src="/jarkol/logo/jarkol.png" alt="Jarkol Technologies">
+                      </NuxtLink>
+                    </div>
+                    <div class="text-sm">
+                        &copy; {{ new Date().getFullYear() }} Jarkol Techonologies S.A. de C.V. Todos los derechos reservados. Diseñado por <NuxtLink target="_blank"
                             to="#" class="font-bold text-primary">Didier Crespo</NuxtLink>
                     </div>
 
                     <div class="flex items-center gap-x-3">
-                        <NuxtLink target="_blank" to="#" class="transition hover:!text-primary">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="w-5 h-5" viewBox="0 0 16 16">
-                                <path
-                                    d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z">
-                                </path>
-                            </svg>
+                        <NuxtLink target="_blank" to="https://www.facebook.com/JarkolTech" class="transition hover:!text-primary">
+                          <svg class="w-[45px] h-[45px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path fill-rule="evenodd" d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z" clip-rule="evenodd"/>
+                          </svg>
                         </NuxtLink>
-
-                        <NuxtLink target="_blank" to="https://github.com/johnkat-mj" class="transition hover:!text-primary">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                class="w-5 h-5" viewBox="0 0 16 16">
-                                <path
-                                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z">
-                                </path>
-                            </svg>
+                        <NuxtLink target="_blank" to="https://api.whatsapp.com/send?phone=529931603161" class="transition hover:!text-primary">
+                          <svg class="w-[45px] h-[45px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path fill="currentColor" fill-rule="evenodd" d="M12 4a8 8 0 0 0-6.895 12.06l.569.718-.697 2.359 2.32-.648.379.243A8 8 0 1 0 12 4ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.96 9.96 0 0 1-5.016-1.347l-4.948 1.382 1.426-4.829-.006-.007-.033-.055A9.958 9.958 0 0 1 2 12Z" clip-rule="evenodd"/>
+                            <path fill="currentColor" d="M16.735 13.492c-.038-.018-1.497-.736-1.756-.83a1.008 1.008 0 0 0-.34-.075c-.196 0-.362.098-.49.291-.146.217-.587.732-.723.886-.018.02-.042.045-.057.045-.013 0-.239-.093-.307-.123-1.564-.68-2.751-2.313-2.914-2.589-.023-.04-.024-.057-.024-.057.005-.021.058-.074.085-.101.08-.079.166-.182.249-.283l.117-.14c.121-.14.175-.25.237-.375l.033-.066a.68.68 0 0 0-.02-.64c-.034-.069-.65-1.555-.715-1.711-.158-.377-.366-.552-.655-.552-.027 0 0 0-.112.005-.137.005-.883.104-1.213.311-.35.22-.94.924-.94 2.16 0 1.112.705 2.162 1.008 2.561l.041.06c1.161 1.695 2.608 2.951 4.074 3.537 1.412.564 2.081.63 2.461.63.16 0 .288-.013.4-.024l.072-.007c.488-.043 1.56-.599 1.804-1.276.192-.534.243-1.117.115-1.329-.088-.144-.239-.216-.43-.308Z"/>
+                          </svg>
                         </NuxtLink>
                     </div>
                 </div>
             </AtomsContainer>
         </div>
-        -->
     </footer>
 </template>
 
