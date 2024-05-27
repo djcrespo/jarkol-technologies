087f301ac5f98a5705d490b61bcaafef45e03cf3
DJCrespo <dj.crespo.castilla@gmail.com>
2024-05-27 17:36:46 -0600

    fix bug in products section

--- components/cards/PrincipalProductCard.vue
+++ components/cards/PrincipalProductCard.vue
@@ -13,7 +13,7 @@
 
 <template>
   <!--
-  <a :data-modal-target="'modal-' + index" :data-modal-toggle="'modal-' + index" class="animate-fade">
+  <a :data-modal-target="'modal-prin-prod-' + index" :data-modal-toggle="'modal-prin-prod-' + index" class="animate-fade">
     <div class="transition duration-300 ease-in-out hover:scale-110">
       <div class="flex justify-center items-center">
         <img class="rounded-t-lg" :src="image" :alt="title" height="50%" />
@@ -36,7 +36,7 @@
       <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
         {{ short_description }}
       </p>
-      <a :data-modal-target="'modal-' + index" :data-modal-toggle="'modal-' + index" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      <a :data-modal-target="'modal-prin-prod-' + index" :data-modal-toggle="'modal-prin-prod-' + index" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Ver más
         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -46,7 +46,7 @@
   </div>
 
   <!-- Main modal -->
-  <div :id="'modal-' + index" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full animate-fade-down animate-duration-700">
+  <div :id="'modal-prin-prod-' + index" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full animate-fade-down animate-duration-700">
     <div class="relative p-4 w-full max-w-2xl max-h-full">
       <!-- Modal content -->
       <div class="relatived bg-white rounded-lg shadow dark:bg-gray-700">
@@ -55,7 +55,7 @@
           <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
             {{ title }}
           </h3>
-          <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" :data-modal-hide="'modal-' + index">
+          <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" :data-modal-hide="'modal-prin-prod-' + index">
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
             </svg>
@@ -78,8 +78,8 @@
         </div>
         <!-- Modal footer
         <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-          <button :data-modal-hide="'modal-' + index" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-          <button :data-modal-hide="'modal-' + index" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
+          <button :data-modal-hide="'modal-prin-prod-' + index" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+          <button :data-modal-hide="'modal-prin-prod-' + index" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
         </div>
         -->
       </div>
