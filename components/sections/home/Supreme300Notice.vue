3038dd1bc0e8b7d1789c54e14b0a410cdff900d7
DJCrespo <dj.crespo.castilla@gmail.com>
2024-05-28 09:14:12 -0600

    update images in novedades section

--- components/sections/home/Supreme300Notice.vue
+++ components/sections/home/Supreme300Notice.vue
@@ -2,7 +2,7 @@
   <AtomsContainer>
     <div class="flex justify-center items-center lg:pt-10 md:pt-10 pt-10 transition duration-300 ease-in-out hover:scale-110">
       <a href="/Supreme300" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <img class="object-cover w-full rounded-t-lg w-full md:h-auto md:w-[18rem] md:rounded-none md:rounded-s-lg" src="/jarkol/noticies/supreme300.jpg" alt="">
+        <img class="object-cover w-full rounded-t-lg w-full md:h-auto md:w-[18rem] md:rounded-none md:rounded-s-lg" src="/jarkol/novedades/Supreme300.png" alt="">
         <div class="flex flex-col justify-between p-4 leading-normal">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Descubre SUPREME300 de Jarkol Technologies</h5>
           <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">La Revolución en Monitoreo Geotérmico.</p>
