8a17c41d4fc06f516b5b6745f646ff73850ac630
DJCrespo <dj.crespo.castilla@gmail.com>
2024-06-01 21:33:27 -0600

    add traduction in services

--- components/sections/servicesCompany/Gallery.vue
+++ components/sections/servicesCompany/Gallery.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import {computed, onMounted} from 'vue'
 import { initFlowbite } from 'flowbite'
 import ProductCard from "~/components/sections/products/ProductCard.vue";
-import services from "public/jarkol/services/services2.json"
+import services_es from "public/jarkol/services/services2_es.json"
+import services_en from "public/jarkol/services/services2_en.json"
 import Service2Card from "~/components/cards/Service2Card.vue";
+import {useI18n} from "vue-i18n";
+
+const { t, locale } = useI18n()
+
+const currentLocale = computed(() => locale.value)
 
 // initialize components based on data attribute selectors
 onMounted(() => {
@@ -15,7 +21,7 @@ onMounted(() => {
   <section class="lg:pt-5 lg:pb-20 md:pt-24 md:pb-24">
     <AtomsContainer>
       <div class="grid md:grid-cols-2 gap-4">
-        <div v-for="(service, index) in services" :key="index">
+        <div v-for="(service, index) in (currentLocale === 'es' ? services_es : services_en)" :key="index">
           <Service2Card
               :title="service.name"
               :index="index"
