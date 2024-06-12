aab38190895cfc00eb158310ee771a1007b31598
DJCrespo <dj.crespo.castilla@gmail.com>
2024-06-12 17:30:06 -0600

    update project

--- components/sections/home/GalleryOpportunities.vue
+++ components/sections/home/GalleryOpportunities.vue
@@ -6,7 +6,7 @@
       <div
           class="absolute inset-0 flex flex-col justify-center text-center text-white text-xl lg:text-4xl md:text-3xl sm:text-6xl font-bold p-4">
         <div class="max-w-full max-h-72 overflow-hidden break-words">
-          {{ t('home.gallery_op.text_1') }}
+          <p class="my-2">{{ t('home.gallery_op.text_1') }}</p>
         </div>
       </div>
     </div>
@@ -18,31 +18,31 @@
       <div
           class="absolute inset-0 flex justify-center items-center text-center text-white text-xl lg:text-4xl md:text-3xl sm:text-4xl font-bold p-4">
         <div class="max-w-full overflow-hidden break-words">
-          {{ t('home.gallery_op.text_2') }}
+          <p class="my-2">{{ t('home.gallery_op.text_2') }}</p>
         </div>
       </div>
     </div>
 
     <div class="relative group overflow-hidden">
-      <img src="/jarkol/GalleryOpportunities/3.jpg"
+      <img src="/jarkol/GalleryOpportunities/5.jpg"
            class="block transition duration-300 ease-in-out group-hover:scale-110" alt="Jarkol Technologies" />
       <div class="absolute inset-0 bg-black opacity-30"></div>
       <div
           class="absolute inset-0 flex justify-center items-center text-center text-white text-xl lg:text-4xl md:text-3xl sm:text-4xl font-bold p-4">
         <div class="max-w-full overflow-hidden break-words">
-          {{ t('home.gallery_op.text_3') }}
+          <p  class="my-2">{{ t('home.gallery_op.text_3') }}</p>
         </div>
       </div>
     </div>
 
     <div class="relative group overflow-hidden">
-      <img src="/jarkol/GalleryOpportunities/4.jpg"
+      <img src="/jarkol/GalleryOpportunities/6.jpeg"
            class="block transition duration-300 ease-in-out group-hover:scale-110" alt="Jarkol Technologies" />
       <div class="absolute inset-0 bg-black opacity-30"></div>
       <div
           class="absolute inset-0 flex justify-center items-center text-center text-white text-xl lg:text-4xl md:text-3xl sm:text-3xl font-bold p-4">
         <div class="max-w-full overflow-hidden break-words">
-          {{ t('home.gallery_op.text_4') }}
+          <p class="my-2">{{ t('home.gallery_op.text_4') }}</p>
         </div>
       </div>
     </div>
