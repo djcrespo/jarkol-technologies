e651a80ce65ea5a6c87abfbb909ad943853a790c
DJCrespo <dj.crespo.castilla@gmail.com>
2024-06-27 22:34:03 -0600

    fix bug on drone page route

--- components/sections/home/NoticeDrone.vue
+++ components/sections/home/NoticeDrone.vue
@@ -1,7 +1,8 @@
 <template>
   <AtomsContainer>
     <div class="flex justify-center items-center lg:pt-10 md:pt-10 pt-10 transition duration-300 ease-in-out hover:scale-110">
-      <a href="/drones" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+      <a href="/Drones"
+         class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
         <img class="object-cover w-full rounded-t-lg w-full md:h-auto md:w-[18rem] md:rounded-none md:rounded-s-lg" src="/jarkol/novedades/dji-matrice.png" alt="">
         <div class="flex flex-col justify-between p-4 leading-normal">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
